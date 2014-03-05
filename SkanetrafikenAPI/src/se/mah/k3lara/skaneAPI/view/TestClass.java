7631ade4236b53dda88236bbed1502b4a89e2691
Lars <K3LARRA@HOTMAIL.COM>
2014-03-05 17:29:25 +0200

    Wrote testclass details

--- SkanetrafikenAPI/src/se/mah/k3lara/skaneAPI/view/TestClass.java
+++ SkanetrafikenAPI/src/se/mah/k3lara/skaneAPI/view/TestClass.java
@@ -13,26 +13,22 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		System.out.println("// Results when searching for Sölvesborg resecentrum - Malmö Gustav Adolfs torg");
-//		String searchURL = Constants.getURL("10001","80100",20); //Malmö C = 80000,  Malmö GAtorg 80100, Häsleholm C 93070
-//		Journeys journeys = Parser.getJourneys(searchURL);
-//		for (Journey journey : journeys.getJourneys()) {
-//			System.out.print(journey.getStartStation()+" - ");
-//			System.out.print(journey.getEndStation());
-//			System.out.println(" Departs in "+journey.getTimeToDeparture()+ " minutes");
-//		} 
-//		
-//	   System.out.println("// Stations when searching for stations containing \"Malm\"");
-//		ArrayList<Station> searchStations = new ArrayList<Station>(); 
-//		searchStations.addAll(Parser.getStationsFromURL("Malm"));
-//		for (Station s: searchStations){
-//			System.out.println(s.getStationName());
-//		}
+		System.out.println("// Results when searching for Sölvesborg resecentrum - Malmö Gustav Adolfs torg");
+		String searchURL = Constants.getURL("10001","80100",20); //Malmö C = 80000,  Malmö GAtorg 80100, Häsleholm C 93070
+		Journeys journeys = Parser.getJourneys(searchURL);
+		for (Journey journey : journeys.getJourneys()) {
+			System.out.print(journey.getStartStation()+" - ");
+			System.out.print(journey.getEndStation());
+			System.out.println(" Departs in "+journey.getTimeToDeparture()+ " minutes");
+		} 
+		
+	   System.out.println("// Stations when searching for stations containing \"Malm\"");
+		ArrayList<Station> searchStations = new ArrayList<Station>(); 
+		searchStations.addAll(Parser.getStationsFromURL("Malm"));
+		for (Station s: searchStations){
+			System.out.println(s.getStationName());
+		}
 
-		//Thingspeaktest
-		XMLParser parser = new XMLParser();
-		String result = parser.getXmlFromUrl( "http://api.thingspeak.com/update?key=EDN7O90L314XP5NT&field1=723&field2=34&field3=25");
-		System.out.println(result);
 	}
 
 }
