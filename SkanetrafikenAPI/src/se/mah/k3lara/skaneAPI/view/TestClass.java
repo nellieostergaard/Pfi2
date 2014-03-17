fb43eeb9f58cdb8f288b41e354b7af245edb498b
K3LARRA <K3LARA@HOTMAIL.COM>
2014-03-17 11:34:29 +0100

    Lite ändringar

--- SkanetrafikenAPI/src/se/mah/k3lara/skaneAPI/view/TestClass.java
+++ SkanetrafikenAPI/src/se/mah/k3lara/skaneAPI/view/TestClass.java
@@ -1,6 +1,7 @@
 package se.mah.k3lara.skaneAPI.view;
 
 import java.util.ArrayList;
+
 import se.mah.k3lara.skaneAPI.model.Journey;
 import se.mah.k3lara.skaneAPI.model.Journeys;
 import se.mah.k3lara.skaneAPI.model.Station;
@@ -11,6 +12,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("// Results when searching for Sölvesborg resecentrum - Malmö Gustav Adolfs torg");
 		String searchURL = Constants.getURL("10001","80100",20); //Malmö C = 80000,  Malmö GAtorg 80100, Häsleholm C 93070 Kolla skånetrafiken för fler koder
+		System.out.println(searchURL);
 		Journeys journeys = Parser.getJourneys(searchURL);
 		for (Journey journey : journeys.getJourneys()) {
 			System.out.print(journey.getStartStation()+" - ");
